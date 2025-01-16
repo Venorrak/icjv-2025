209518fb5e3cc1609e7be9ed89e728047bd7d8f8
Liam <liamdoyon@hotmail.com>
2025-01-16 01:30:59 -0500

    progress_bar

--- Scenes/levels/bar.gd
+++ Scenes/levels/bar.gd
@@ -1,10 +1,12 @@
 extends Sprite2D
 class_name Bar
 
-
+@export var progressBar: ProgressBar
 @export var maxSpeed : float = -300
 @export var acceleration := 0.01
 @export var fallSpeed : float = 300
+var progress: float = 0
+@export var progressSpeed: float = 15
 
 var player: Player
 var currentSpeed : float = 0
@@ -14,6 +16,7 @@ var random_y:float
 
 func _ready() -> void:
 	player = get_parent().get_node("Player")
+	progressBar = get_parent().get_node("ProgressBar")
 
 func _process(delta: float) -> void:
 	
@@ -30,9 +33,15 @@ func _process(delta: float) -> void:
 
 	if(position.y >= player.position.y && position.y <= player.position.y + player.size.y):
 		print("JE SUIS LA")
+		progress += progressSpeed * delta
+		progress = clamp(progress, 0, 100)
+		progressBar.value = progress
 	else:
-		print("MOI PAS LA")
-	
+		print(progress)
+		progress -= progressSpeed * delta
+		progress = clamp(progress, 0, 100)
+		progressBar.value = progress
+
 func _on_timer_timeout() -> void:
 	random_y  = randf_range(-500.0,500.0)
 
