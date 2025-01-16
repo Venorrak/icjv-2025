e7389832ce0147ff74ac3b30a3445215538ad0c4
Venorrak <112645452+Venorrak@users.noreply.github.com>
2025-01-16 17:14:57 -0500

    replaced and polished loop

--- Code/Scripts/morgue.gd
+++ Code/Scripts/morgue.gd
@@ -3,13 +3,6 @@ extends Node2D
 @export var deathScene : PackedScene
 @export var spawnHelpersBase : Node2D
 
-func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		if nextBody.getParts().size() == 6:
-			GlobalVars.futureCharacter = nextBody.getPlayerSkin()
-			GlobalVars.currentWave += 1
-			SignalBus.newLoop.emit()
-
 func _on_round_timer_timeout() -> void:
 	GlobalVars.reset()
 	get_tree().change_scene_to_packed(deathScene)
--- Code/Scripts/nextBodyManager.gd
+++ Code/Scripts/nextBodyManager.gd
@@ -3,6 +3,9 @@ extends Node2D
 @export var body : Node2D
 var lastBodyState : Array = []
 
+var playerWithin : bool = false
+@export var pressLabel : Sprite2D
+
 func _ready() -> void:
 	SignalBus.connect("setNextBody", setNextBody)
 	
@@ -16,3 +19,19 @@ func _physics_process(delta: float) -> void:
 	if body.playerSkin != lastBodyState:
 		lastBodyState = body.playerSkin
 		SignalBus.nextBodyStatus.emit(body.playerSkin)
+	if Input.is_action_just_pressed("interact") and body.playerSkin.size() == 6 and playerWithin:
+		GlobalVars.futureCharacter = body.getPlayerSkin()
+		GlobalVars.currentWave += 1
+		SignalBus.newLoop.emit()
+
+func _on_area_2d_body_entered(value: Node2D) -> void:
+	if value.is_in_group("player"):
+		playerWithin = true
+		if body.playerSkin.size() == 6:
+			pressLabel.visible = true
+
+func _on_area_2d_body_exited(value: Node2D) -> void:
+	if value.is_in_group("player"):
+		playerWithin = false
+		if body.playerSkin.size() == 6:
+			pressLabel.visible = false
