49e8c63574b3da11bdb205484555ba23e8649364
S-Beaulieu <Beaulieu-Salomon@protonmail.com>
2025-01-16 13:47:00 -0500

    wip

--- Scenes/levels/player.gd
+++ Scenes/levels/player.gd
@@ -4,41 +4,41 @@ class_name Player
 
 var speed: float = 1000.0 
 var direction = Vector2()
+var canMove: bool = true
 
 func _ready() -> void:
 	pass
+
 func _process(delta: float) -> void:
+	if canMove:
+		if Input.is_action_pressed("down"):
+			direction = Vector2(0, speed)
+		elif Input.is_action_pressed("up"):
+			direction = Vector2(0, -speed)
+		elif Input.is_action_pressed("left"):
+			direction = Vector2(-speed, 0)
+		elif Input.is_action_pressed("right"):
+			direction = Vector2(speed, 0)
+		else:
+			direction = Vector2.ZERO 
+
+		if direction != Vector2.ZERO:
+			var next_position = position + direction * delta
+
+			if !is_colliding_with_obstacle(next_position):
+				position = next_position 
+				look_at(position + direction) 
+			else:
+				print("Collision détectée, mouvement bloqué.")
+			
+			canMove = false
+
 	
-	if Input.is_action_pressed("ui_down"):
-		direction = Vector2(0, speed)
-		
-		if $RayCast2D.is_colliding():
-			position+=Vector2(0,0)
-		else:
-			position+=direction*delta
-	elif Input.is_action_pressed("ui_up"):
-		direction = Vector2(0, -speed)
-		
-		if $RayCast2D.is_colliding():
-			position+=Vector2(0,0)
-		else:
-			position+=direction*delta
-	elif Input.is_action_pressed("ui_left"):
-		direction = Vector2(-speed, 0)
-		
-		if $RayCast2D.is_colliding():
-			position+=Vector2(0,0)
-		else:
-			position+=direction*delta
-	elif Input.is_action_pressed("ui_right"):
-		direction = Vector2(speed, 0)
-		if $RayCast2D.is_colliding():
-			position+=Vector2(0,0)
-		else:
-			position+=direction*delta
-	if direction.length() > 0:
-		look_at(position + direction) 
-	print("Direction: ",direction)
-	print("Position X:",position.x)
-	print("Position Y:",position.y)
-	
+	if !is_colliding_with_obstacle(position):
+		canMove = true 
+		print("Direction:", direction)
+		print("Position X:", position.x)
+		print("Position Y:", position.y)
+
+func is_colliding_with_obstacle(next_position: Vector2) -> bool:
+	return $Detection.is_colliding()
