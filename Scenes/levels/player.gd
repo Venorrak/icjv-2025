9fdf2c1bb6e8e03b78ba62b88c2f73511d7efde1
S-Beaulieu <Beaulieu-Salomon@protonmail.com>
2025-01-16 15:33:21 -0500

    wip

--- Scenes/levels/player.gd
+++ Scenes/levels/player.gd
@@ -2,10 +2,11 @@ extends Sprite2D
 
 class_name Player
 
-var speed: float = 1000.0 
+
+var speed: float = 5000.0 
 var direction = Vector2()
 var canMove: bool = true
-
+var last_mouvement = Vector2.ZERO
 func _ready() -> void:
 	pass
 
@@ -13,32 +14,29 @@ func _process(delta: float) -> void:
 	if canMove:
 		if Input.is_action_pressed("down"):
 			direction = Vector2(0, speed)
+			look_at(position + direction) 
+			canMove=false
 		elif Input.is_action_pressed("up"):
 			direction = Vector2(0, -speed)
+			look_at(position + direction) 
+			canMove=false
 		elif Input.is_action_pressed("left"):
 			direction = Vector2(-speed, 0)
+			look_at(position + direction) 
+			canMove=false
 		elif Input.is_action_pressed("right"):
 			direction = Vector2(speed, 0)
-		else:
-			direction = Vector2.ZERO 
-
-		if direction != Vector2.ZERO:
-			var next_position = position + direction * delta
-
-			if !is_colliding_with_obstacle(next_position):
-				position = next_position 
-				look_at(position + direction) 
-			else:
-				print("Collision détectée, mouvement bloqué.")
-			
-			canMove = false
+			look_at(position + direction) 
+			canMove=false
+		print("Collision détectée, mouvement débloqué.")
+		last_mouvement = direction
+		look_at(position + direction)
+		
+	if direction != Vector2.ZERO:
+		while not $Detection.is_colliding():
+			position += last_mouvement * delta
+			print("Aucune Collision détectée, mouvement bloqué.")
+		canMove = true
 
 	
-	if !is_colliding_with_obstacle(position):
-		canMove = true 
-		print("Direction:", direction)
-		print("Position X:", position.x)
-		print("Position Y:", position.y)
-
-func is_colliding_with_obstacle(next_position: Vector2) -> bool:
-	return $Detection.is_colliding()
+	
