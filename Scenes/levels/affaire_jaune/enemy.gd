8e8519ee8844e4d74ed9e85c38deeeba18a3d50b
S-Beaulieu <Beaulieu-Salomon@protonmail.com>
2025-01-16 20:33:08 -0500

    enemy_wip

--- Scenes/levels/affaire_jaune/enemy.gd
+++ Scenes/levels/affaire_jaune/enemy.gd
@@ -1,28 +1,30 @@
 extends AnimatedSprite2D
 
-var direction := Vector2.ZERO
-var acceleration:=0.01
-var currentSpeed:float=0
-var upSpeed:float = 200
-var downSpeed:float = -200
-
-
+@export var maxSpeed : float = 300
+@export var acceleration := 0.01
+@export var difficulte = 1
+var currentSpeed : float = 0
+var direction = Vector2()
+var goTop :bool=true
 func _ready() -> void:
 	pass
 
-
 func _process(delta: float) -> void:
 	
+	direction = Vector2(0, currentSpeed)
+	if direction != Vector2.ZERO:
+		look_at(position + direction)
 	
-	pass
+	if($Detection.is_colliding()):
+		if(goTop):
+			currentSpeed = lerp(direction, maxSpeed, acceleration)
+			
+			position += direction * delta
+			goTop=false
+			
+		else:
+			position -= direction * delta
+			goTop=true
 
 
-func _on_hit_box_body_entered(body: Node2D) -> void:
-	var area_name := body.name.to_lower()
 	
-	if (area_name == "level"):
-		currentSpeed = lerp(direction.y,upSpeed,acceleration)
-		position+=direction*currentSpeed
-	else:
-		currentSpeed = lerp(direction.y,downSpeed,acceleration)
-	print(area_name)
