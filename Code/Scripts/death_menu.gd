fb780e212b99d6cc91971640e0175ce839f51a82
Venorrak <112645452+Venorrak@users.noreply.github.com>
2025-01-16 21:04:55 -0500

    how to menu finished

--- Code/Scripts/death_menu.gd
+++ Code/Scripts/death_menu.gd
@@ -3,6 +3,7 @@ extends Control
 @export var playButton : TextureButton
 @export var quitButton : TextureButton
 @export var morgue : PackedScene
+@export var menu : PackedScene
 
 func _ready() -> void:
 	playButton.grab_focus()
@@ -12,3 +13,6 @@ func _on_play_button_button_up() -> void:
 
 func _on_quit_button_button_up() -> void:
 	get_tree().quit()
+
+func _on_menu_button_button_up() -> void:
+	get_tree().change_scene_to_packed(menu)
--- Code/Scripts/main_menu.gd
+++ Code/Scripts/main_menu.gd
@@ -1,15 +1,21 @@
 extends Control
 @export var playButton : TextureButton
 @export var quitButton : TextureButton
+@export var howToButton : TextureButton
 @export var title : Sprite2D
+@export var howToSection : Control
 
 @export var morgue : PackedScene
 
 var frameMov : int = 1
+var inHow : bool = false
 
 func _ready() -> void:
 	playButton.grab_focus()
 
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel") and inHow:
+		changeVisibility()
 func _on_timer_timeout() -> void:
 	if title.frame == title.hframes - 1:
 		frameMov = -1
@@ -22,3 +28,14 @@ func _on_play_button_button_up() -> void:
 
 func _on_quit_button_button_up() -> void:
 	get_tree().quit()
+
+func _on_how_to_button_button_up() -> void:
+	changeVisibility()
+	inHow = true
+
+func changeVisibility() -> void:
+	playButton.visible = !playButton.visible
+	howToButton.visible = !howToButton.visible
+	quitButton.visible = !quitButton.visible
+	title.visible = !title.visible
+	howToSection.visible = !howToSection.visible
