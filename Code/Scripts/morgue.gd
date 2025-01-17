48a3b361b4469ea7e39f256ff14bcb3c12ee9fe6
Venorrak <112645452+Venorrak@users.noreply.github.com>
2025-01-16 22:57:57 -0500

    darkmode in-game

--- Code/Scripts/morgue.gd
+++ Code/Scripts/morgue.gd
@@ -3,6 +3,19 @@ extends Node2D
 @export var deathScene : PackedScene
 @export var spawnHelpersBase : Node2D
 
+@export var imgDayWindow : Texture2D
+@export var imgNightWindow : Texture2D
+@export var dark : Sprite2D
+@export var window : Sprite2D
+
+func _ready() -> void:
+	if GlobalVars.darkMode:
+		dark.visible = true
+		window.texture = imgNightWindow
+	else:
+		dark.visible = false
+		window.texture = imgDayWindow
+
 func _on_round_timer_timeout() -> void:
 	GlobalVars.reset()
 	get_tree().change_scene_to_packed(deathScene)
